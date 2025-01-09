f6fd2332975135bddc7f61ef895f8cc62aab3e00
ranrat <humayunk628@gmail.com>
2025-01-09 14:00:00 -0500

    my first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+
+
+
+
+def conversation():
+	answer = input()
+	if answer == ("Hell yeah"):
+		print("Awesome.")
+	else:
+		print("That's a shame.")
+
+def main():
+	print("This is your first printing code. Are you gonna be able to do better?")
+	conversation()
+	print("Either way, you got this!")
+
+
+if __name__ == "__main__":
+	main()		
